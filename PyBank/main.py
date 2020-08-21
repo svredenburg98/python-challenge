acf4a86ad77cb5fbd758394ffd5b869192ba73e1
svredenburg98 <66885962+svredenburg98@users.noreply.github.com>
2020-08-21 16:08:09 -0500

    Added average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,16 +15,23 @@ with open(csv_path, 'r') as csvfile:
     
     TotalProfitLoss = 0
     ProfitChange = []
+    ProfitInitial = 0
     for rows in reader:
 
         TotalMonths += 1
         Profit = float(rows[1])
         TotalProfitLoss = TotalProfitLoss + Profit
-        
-
+        # Create a list of all profit deltas
+        ProfitChange.append(Profit - ProfitInitial)
+        ProfitInitial = Profit
+    
+    #Ignore first delta 
+    TotalChange = sum(ProfitChange[1:(len(ProfitChange))]) 
+    AverageChange = TotalChange / (len(ProfitChange)-1)
     print("Financial Analysis")
     print("-"*40)
     print(f"Total Months: {TotalMonths}")
     print(f"Total Profit/Loss: {TotalProfitLoss}")
-    
+    #print(ProfitChange)
+    print(f"Average Change: {AverageChange}")
 
